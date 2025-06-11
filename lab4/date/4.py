2b1a390b2caab53dcbdc056b2b7b9e71f37c4cb3
D D <dyanne@MacBook-Air-D.local>
2025-06-11 12:52:09 +0500

    Обновила файл

--- lab4/date/4.py
+++ lab4/date/4.py
@@ -1,11 +1,13 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+def diff(d1, d2):
+    date1 = datetime.strptime(d1, "%Y-%d-%m %H:%M:%S")
+    date2 = datetime.strptime(d2, "%Y-%d-%m %H:%M:%S")
 
-import datetime
+    res = date2 - date1
 
+    ressec = res.total_seconds()
+    return ressec
 
-d1 = datetime.datetime(input("Enter first date: "))
-
-
-d2 = datetime.datetime(input("Enter second date: "))
-
-result = timedelta
+datestr1 = '2020-12-12 12:00:00'
+datestr2 = '2020-12-12 11:00:00'
+print(diff(datestr1, datestr2))
