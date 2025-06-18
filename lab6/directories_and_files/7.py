6724641638cde5a434d2d5b8ef5ed330b5c21c91
D D <dyanne@MacBook-Air-D.local>
2025-06-18 10:23:33 +0500

    Обновила файл

--- lab6/directories_and_files/7.py
+++ lab6/directories_and_files/7.py
@@ -1,6 +1,6 @@
 import os
 
-with open('pp2_spring/lab6/examplefile.txt', 'r') as r:
-    with open('pp2_spring/lab6/examplefile_copy.txt', 'w') as w:
+with open('1.txt', 'r') as r:
+    with open('2.txt', 'w') as w:
         for line in r:
             w.write(line)
