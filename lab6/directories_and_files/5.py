cb7d4ba6ba1cc903d4ccd0ab0dc58f91b3fa83fd
D D <dyanne@MacBook-Air-D.local>
2025-06-18 10:20:02 +0500

    Обновила файл

--- lab6/directories_and_files/5.py
+++ lab6/directories_and_files/5.py
@@ -1,5 +1,5 @@
 import os
-list = list(input().split())
-with open('pp2_spring/lab6/examplefile.txt', 'w') as file:
-    for i in list:
+lst = list(input().split())
+with open('1.txt', 'w') as file:
+    for i in lst:
         file.write(str(i) + ' ')
